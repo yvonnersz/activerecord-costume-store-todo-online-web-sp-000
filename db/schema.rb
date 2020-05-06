4ef203ee61e3bea4598c6fbf9b5cb1d6322cd9f2
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-06 11:40:07 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
-
-  create_table "costume_stores", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.integer "costume_inventory"
-    t.integer "num_of_employees"
-    t.boolean "still_in_business"
-    t.datetime "opening_time"
-    t.datetime "closing_time"
-  end
+ActiveRecord::Schema.define(version: 1) do
 
   create_table "costumes", force: :cascade do |t|
     t.string "name"
